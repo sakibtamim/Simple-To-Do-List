ed85b321af13244a7d1ecbbc031586575a752c47
sakibtamim <sakib.tamim@gmail.com>
2025-09-23 17:22:06 +0600

    feat: improve responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -35,43 +35,43 @@ const App = () => {
   };
 
   return (
-    <section className="w-[600px] mx-auto pt-1">
-      <div className="w-[600px] h-fit rounded-md bg-bg">
-        <div className="pt-[10px] pb-[20px] text-[40px] text-box font-semibold text-center">
+    <section className="lg:w-[600px] w-[400px] mx-auto pt-1  ">
+      <div className="lg:w-[600px] w-[400px] h-fit rounded-md bg-bg">
+        <div className="lg:pt-[10px] pt-[6px] lg:pb-[20px] pb-[8px] lg:text-[40px] text-[24px] text-box font-semibold text-center">
           To-Do List
         </div>
         <div className="p-2 relative">
           <input
             type="text"
             value={inputValue}
-            className=" text-[30px] font-semibold text-inputtext pl-2 py-[15px] w-full bg-input rounded-md border-none outline-none"
+            className=" lg:text-[30px] text-[20px] font-semibold text-inputtext pl-2 lg:py-[15px] py-[10px] w-full bg-input rounded-md border-none outline-none"
             onChange={(e) => setInputValue(e.target.value)}
             onKeyDown={handleEnter}
           />
           <div
-            className="absolute top-1/2 -translate-y-1/2 right-[20px]"
+            className="absolute top-1/2 -translate-y-1/2 lg:right-[20px] right-[16px]"
             onClick={handleAdd}
           >
-            <LuPlus className="text-inputtext text-[50px]" />
+            <LuPlus className="text-inputtext lg:text-[50px] text-[30px]" />
           </div>
         </div>
 
         {list.map((item, index) => (
           <div key={item} className="relative p-2">
-            <p className=" w-full bg-box text-[30px] pl-[8px] py-[10px]  rounded-md ">
+            <p className=" w-full bg-box lg:text-[30px] text-[16px] pl-[8px] lg:py-[10px] py-[8px] rounded-md ">
               {item}
             </p>
             <div
-              className="absolute top-1/2 -translate-y-1/2 right-[60px] "
+              className="absolute top-1/2 -translate-y-1/2 lg:right-[60px] right-[40px] "
               onClick={() => handleUpdate(item, index)}
             >
-              <LuPlus className="text-inputtext text-[38px]" />
+              <LuPlus className="text-inputtext lg:text-[38px] text-[20px]" />
             </div>
             <div
-              className="absolute top-1/2 -translate-y-1/2 right-[20px]"
+              className="absolute top-1/2 -translate-y-1/2 lg:right-[20px] right-[16px]"
               onClick={() => handleDelete(index)}
             >
-              <RxCross2 className="text-inputtext text-[38px]" />
+              <RxCross2 className="text-inputtext lg:text-[38px] text-[20px]" />
             </div>
           </div>
         ))}
