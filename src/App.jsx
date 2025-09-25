87a1a3cd93252976ae54cc2d755dd3b0155e6e06
sakibtamim <sakib.tamim@gmail.com>
2025-09-25 20:59:45 +0600

    feat: add lcoal storage in todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LuPlus } from "react-icons/lu";
 import { MdEdit } from "react-icons/md";
 import { RxCross2 } from "react-icons/rx";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(() => {
+    const saveList = localStorage.getItem("todos");
+    return saveList ? JSON.parse(saveList) : [];
+  });
   const [editIndex, setEditIndex] = useState(null);
 
   let handleAdd = () => {
@@ -34,6 +37,10 @@ const App = () => {
     const newList = list.filter((_, i) => i !== index);
     setList(newList);
   };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(list));
+  }, [list]);
 
   return (
     <section className="lg:w-[600px] w-[400px] mx-auto lg:pt-1  ">
